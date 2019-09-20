00df6d6b0be52e0b59eb8383898ba152150bf672
tangxiumei <tangxiumei@header.com.cn>
2019-09-20 13:47:14 +0800

    init project

---
+++ src/components/button/index.vue
@@ -0,0 +1,23 @@
+<template>
+ <button>测试组件</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  data() {
+    return {}
+  },
+  computed: {},
+
+  watch: {},
+
+  created() {},
+
+  mounted() {
+  },
+
+  methods: {
+  }
+}
+</script>
